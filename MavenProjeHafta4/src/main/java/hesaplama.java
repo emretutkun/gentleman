46f0cd0acf95283a401c17457e3eb270e257edc9
bprlab <bprlab@10.200.144.96>
2018-03-17 14:31:49 +0300

    Mven proje hafta 4

---
+++ MavenProjeHafta4/src/main/java/hesaplama.java
@@ -0,0 +1,8 @@
+
+public class hesaplama {
+	public static int kareAlma(int sayi) {
+		return sayi*sayi;
+		
+	}
+
+}
---
+++ MavenProjeHafta4/src/test/java/HesaplamaTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class HesaplamaTest {
+	@Test
+	public void testKareAlma() {
+		Assert.assertEquals(100,hesaplama.kareAlma(10));
+		
+	}
+	
+
+}
